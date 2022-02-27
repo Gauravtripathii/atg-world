935145b181764509d3bffd2c8b7477eea89679d9
Gauravtripathii <gauravtripathii7979@gmail.com>
2022-02-27 09:13:53 +0530

    finnal mobile view commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import Header from "./components/Header";
 import Posts from "./components/Posts";
+import Register from "./components/Register";
 
 import "./style/header.css";
 import "./style/posts.css";
 import "./style/post.css";
+import "./style/register.css";
 
 function App() {
   return (
     <div className="App">
+      <Register />
       <Header />
       <Posts />
     </div>
---
+++ src/components/Register.js
@@ -0,0 +1,53 @@
+import cancel_icon from "../media/icons/cancel.png";
+import facebook_icon from "../media/icons/facebook.png";
+import google_icon from "../media/icons/google.png";
+
+function Register() {
+  const close_register_box = () => {
+    var reg = document.querySelector(".register");
+    reg.style.height = "0vh";
+  };
+  return (
+    <div className="register">
+      <div className="head">
+        <h3>Create Account</h3>
+        <div className="cancel" onClick={() => close_register_box()}>
+          <img src={cancel_icon} alt="cancle icon" />
+        </div>
+      </div>
+      <form>
+        <p className="name">
+          <input type="text" placeholder="First Name" />
+          <input type="text" placeholder="Last Name" />
+        </p>
+        <p>
+          <input type="text" placeholder="Email" />
+        </p>
+        <p>
+          <input type="text" placeholder="Password" />
+        </p>
+        <p>
+          <input type="text" placeholder="Confirm Password" />
+        </p>
+        <p className="buttons">
+          <button>Create Account</button>
+          <a href="/">or,SignIn</a>
+        </p>
+      </form>
+      <div className="socials">
+        <p>
+          <img src={facebook_icon} alt="facebook icon" /><span>Sign up with Facebook</span>
+        </p>
+        <p>
+          <img src={google_icon} alt="google icon" /><span>Sign up with Google</span>
+        </p>
+      </div>
+      <footer>
+        By signing up, you agree to our Terms & <br />
+        conditions, Privacy policy
+      </footer>
+    </div>
+  );
+}
+
+export default Register;
